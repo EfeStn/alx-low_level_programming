0d309672b4dc035bb6887babfd6264e54982c77b
EfeStn <efeurhieme11@gmail.com>
2023-08-16 07:53:46 -0700

    alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,14 @@
 
 /**
  * print_alphabet - prints the alphabet in lowercase
+ *
  * Return: 0 (Success)
  */
 void print_alphabet(void)
 {
 	char ch;
 
-	ch = 'a';
-
-	while (ch <= 'z')
-	{
-		_putchar(ch);
-		ch++;
-	}
-
+	for (ch = 'a'; ch <= 'z'; ch++)
+	_putchar (ch);
 	_putchar('\n');
 }
