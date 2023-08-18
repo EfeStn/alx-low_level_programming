cc41a7f2522375366d099b38a2c82af2b8c0fdca
EfeStn <efeurhieme11@gmail.com>
2023-08-17 17:24:41 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,13 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	void print_triangle(void);
+	void print_triangle(void);
+	void print_triangle(void);
+	void print_triangle(void);
+	return (0);
+}
