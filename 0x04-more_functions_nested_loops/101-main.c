c8e4fee7afa75771a1619f3f05d7f8c901c9f4bc
EfeStn <efeurhieme11@gmail.com>
2023-08-17 17:38:28 -0700

    Numbers have life; they're not just symbols on paper

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,19 @@
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	void print_number(void);
+	void _putchar(void);
+	void print_number(void);
+	void _putchar(void);
+	void print_number(void);
+	void _putchar(void);
+	void print_number(void);
+	void _putchar(void);
+	void print_number(void);
+	void _putchar(void);
+	return (0);
+}
