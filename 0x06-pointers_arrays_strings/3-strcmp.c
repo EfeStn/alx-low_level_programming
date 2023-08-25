0c42956e4e1443f33ee3d741fe94aa5d86f76ea4
EfeStn <efeurhieme11@gmail.com>
2023-08-24 17:45:28 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _stremp - Compare two strings
+ * @s1: First string to compare
+ * @s2: Second string to compare
+ * Return: 0 if strings are equal,
+ * positive if s1 is greater, negative if s2 is greater
+ */
+int _stremp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+		{
+			return (*s1 - *s2);
+		}
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
