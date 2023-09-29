3766b7d28fde9172f0a4a5e0e6e37d979262c343
EfeStn <efeurhieme11@gmail.com>
2023-09-29 06:29:07 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,19 +10,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	if (b == NULL)
-		return (0);
-
+	int i;
 	unsigned int result = 0;
 
-	for (int i = 0; b[i] != '\0'; i++)
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
+	if (!b)
+		return (0);
 
-		result = result * 2 + (b[i] - '0');
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		result = 2 * result + (b[i] - '0');
 	}
 
 	return (result);
 }
-
