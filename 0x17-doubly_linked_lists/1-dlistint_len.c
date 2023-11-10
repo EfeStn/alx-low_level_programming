1f2bf0154f87be0b82123a0a0a31a507cae971b0
EfeStn <efeurhieme11@gmail.com>
2023-11-10 12:12:04 -0800

    List length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -5,7 +5,7 @@
  * @h: The head of the dlistint_t list.
  * Return: The number of elements in the list.
  *
- * Description: This function returns the number of elements in a dlistint_t list.
+ * Description: This function returns the number of elements in a list.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
